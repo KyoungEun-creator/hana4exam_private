1ed0e74047dbfef175333a2fc49a66c8634e1dd0
Kyoungeun Jo <kavvy99@naver.com>
2024-09-11 11:30:25 +0900

    xx

--- FEBasic/조경은/ex3.js
+++ FEBasic/조경은/ex3.js
@@ -1,3 +1,34 @@
-Array.prototype.sortBy = function (sortProp = '') {
-  return this;
+Array.prototype.sortBy = function (sortProp = "") {
+  // name | name:desc | name:asc
+  if (sortProp.includes(",")) {
+    const [...condition] = sortProp.split(",");
+    console.log("🚀   condition1:", condition);
+    // console.log("🚀   condition2:", condition2);
+  } else {
+    console.log("🚀   condition0:", sortProp);
+    const [key, direction = "asc"] = sortProp?.split(":");
+    const dir = direction.toLowerCase() === "desc" ? -1 : 1;
+    return console.log(this.sort((a, b) => (a[key] > b[key] ? dir : -dir)));
+  }
 };
+const hong = { id: 1, name: "Hong", city: "Busan", dept: 1 };
+const kim = { id: 2, name: "Kim", city: "Seoul", dept: 2 };
+const lee = { id: 3, name: "Lee", city: "Daegu", dept: 2 };
+const users = [lee, hong, kim];
+
+users.sortBy("id"); // [hong, kim, lee]
+users.sortBy("name:desc,id:,dept:desc"); // [lee, kim, hong];
+
+// const hong = { id: 1, name: "Hong", city: "Busan", dept: 1 };
+// const kim = { id: 2, name: "Kim", city: "Seoul", dept: 2 };
+// const lee = { id: 3, name: "Lee", city: "Daegu", dept: 2 };
+// const users = [lee, hong, kim];
+
+// const assert = require("assert");
+
+// assert.deepStrictEqual(users.sortBy("id"), [hong, kim, lee]);
+// assert.deepStrictEqual(users.sortBy("name:desc"), [lee, kim, hong]);
+// assert.deepStrictEqual(users.sortBy("dept:desc,city:asc"), [lee, kim, hong]);
+// assert.deepStrictEqual(users.sortBy("dept:desc,city:desc"), [kim, lee, hong]);
+// assert.deepStrictEqual(users.sortBy("name:desc,id:,dept:desc"), [lee, kim, hong]);
+// assert.deepStrictEqual(users.sortBy("dept:desc,id"), [kim, lee, hong]);
--- FEBasic/조경은/ex8.ts
+++ FEBasic/조경은/ex8.ts
@@ -1,4 +1,5 @@
 // dummy(mock)입니다. 올바르게 수정하세요.
+// type any -> cb에 어느 타입이 와도 수용 가능하게 범용적으로 만들기
 const debounce = (cb: any, delay: number) => (i: number) => {};
 const throttle = (cb: any, delay: number) => (i: number) => {};
 
@@ -9,3 +10,23 @@ for (let i = 10; i < 15; i++) debo(i); // 15 출력
 
 const thro = throttle((a: number) => console.log(a + 1), 500);
 for (let i = 10; i < 15; i++) thro(i); // 11 출력
+
+// ---------------------
+export const debounce = (cb, delay) => {
+  let timer;
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(cb, delay, ...args);
+  };
+};
+
+export const throttle = (cb, delay) => {
+  let timer;
+  return (...args) => {
+    if (timer) return;
+    timer = setTimeout(() => {
+      cb(...args);
+      timer = null;
+    }, delay);
+  };
+};
