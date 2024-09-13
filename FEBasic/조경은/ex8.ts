375d3bdbdb0b4ba3d5e268156b314ae710395ca4
Kyoungeun Jo <kavvy99@naver.com>
2024-09-13 13:39:04 +0900

    ex8 - add type in timer

--- FEBasic/조경은/ex8.ts
+++ FEBasic/조경은/ex8.ts
@@ -1,23 +1,19 @@
-// dummy(mock)입니다. 올바르게 수정하세요.
-// const debounce = (cb: any, delay: number) => (i: number) => {};
-// const throttle = (cb: any, delay: number) => (i: number) => {};
+type TCb = (i: number) => void;
 
-// ---------------------
-
-export const debounce = <T extends unknown[]>(cb: (...args: T) => void, delay: number) => {
-  let timer: NodeJS.Timeout;
-  return (...args: T) => {
+const debounce = (cb: TCb, delay: number) => {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  return (i: number) => {
     if (timer) clearTimeout(timer);
-    timer = setTimeout(() => cb(...args), delay);
+    timer = setTimeout(() => cb(i), delay);
   };
 };
 
-export const throttle = <T extends unknown[]>(cb: (...args: T) => void, delay: number) => {
-  let timer: NodeJS.Timeout | null;
-  return (...args: T) => {
+const throttle = (cb: TCb, delay: number) => {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  return (i: number) => {
     if (timer) return;
     timer = setTimeout(() => {
-      cb(...args);
+      cb(i);
       timer = null;
     }, delay);
   };
@@ -25,7 +21,9 @@ export const throttle = <T extends unknown[]>(cb: (...args: T) => void, delay: n
 
 // test code
 const debo = debounce((a: number) => console.log(a + 1), 500);
-for (let i = 10; i < 15; i += 1) console.log(debo(i)); // 15 출력
+for (let i = 10; i < 15; i++) debo(i); // 15 출력
 
 const thro = throttle((a: number) => console.log(a + 1), 500);
-for (let i = 10; i < 15; i += 1) console.log(thro(i)); // 11 출력
+for (let i = 10; i < 15; i++) thro(i); // 11 출력
+
+export { debounce, throttle };
